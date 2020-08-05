2e69732dc5deec31c42c75e8f170b3304b868104
Nicklas Pedersen (nick8186) <nick8186@zbc.dk>
2020-08-05 19:08:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBHLufthavn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new testEntities())
+            {
+                airport kbh = new airport { iata_code = "KBH", name = "Copenhagen airport" };
+                airport la = new airport { iata_code = "LAX", name = "Los Angeles airport" };
+                airline airline = new airline { name = "SAS" };
+                airline airline2 = new airline { name = "ASA" };
+                air_route route = new air_route { airline = airline, airline1 = airline2, airport = new List<airport> { la, kbh } };
+                ctx.air_route.Add(route);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
